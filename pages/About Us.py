5b55a1b343bb717140243756727d527951adaca4
Nithinkumar-29:::wq! <vangalanithinkumarreddy@gmail.com>
2023-05-16 19:55:58 +0530

    about us

--- pages/About Us.py
+++ pages/About Us.py
@@ -1,11 +1,11 @@
 import streamlit as st
-col1,col2,col3 = st.columns([2,1,2])
+col1,col2,col3 = st.columns([3,1,3])
 with col2:
-	st.markdown("# About Us")
+	st.header("About Us")
 st.divider()
-col1,col2,col3 = st.columns([1,9,1])
+col1,col2,col3 = st.columns([1,20,1])
 with col2:
-	st.markdown("#### We are a team driven by the problems that surround us and strive to provide a solution accessible to all.")
+	st.markdown("##### We are a team driven by the problems that surround us and strive to provide a solution accessible to all. Our mission with this application is to provide a free-for-all service that enables users to know the stage of Alzheimer's Disease based on the uploaded MRI scan and suggestions to maintain your health based on the result given.")
 st.divider()
 st.subheader("Introducing the team behind the project")
 col1,col2,col3 = st.columns([2,1,2])
