c71a117f0f027ebc91782a731b5c0cc7740bafb6
Nithinkumar-29 <105154379+Nithinkumar-29@users.noreply.github.com>
2023-05-16 19:59:30 +0530

    Update About Us.py

--- pages/About Us.py
+++ pages/About Us.py
@@ -7,7 +7,7 @@ col1,col2,col3 = st.columns([1,20,1])
 with col2:
 	st.markdown("##### We are a team driven by the problems that surround us and strive to provide a solution accessible to all. Our mission with this application is to provide a free-for-all service that enables users to know the stage of Alzheimer's Disease based on the uploaded MRI scan and suggestions to maintain your health based on the result given.")
 st.divider()
-st.subheader("Introducing the team behind the project")
+st.subheader("Introducing the team behind the project:")
 col1,col2,col3 = st.columns([2,1,2])
 with col2:
 	st.image("Jagan.jpg")
